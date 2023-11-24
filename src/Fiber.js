3440a2e13e4a5c88194f5169a4860297e062a6a9
Besco <bernardbolter@gmail.com>
2023-11-24 14:11:35 +0100

    added text test

--- src/Fiber.js
+++ src/Fiber.js
@@ -1,12 +1,20 @@
-import { useRef } from 'react'
+import { useState, useRef } from 'react'
 import { ARView, ARAnchor} from 'react-three-mind'
 // import Target from './targets.mind'
 
 const Fiber = () => {
-    const ref= useRef()
+    const ref = useRef()
+    const [showText, setShowText] = useState(false)
+    const anchorFound = () => {
+        setShowText(true)
+    }
+    const anchorLost = () => {
+        setShowText(false)
+    }
     return (
         <div className="fiber-container">
             <ARView
+                ref={ref}
                 imageTargets="./targets.mind"
                 filterMinCF={1}
                 filterBeta={10000}
@@ -15,13 +23,20 @@ const Fiber = () => {
             >
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <ARAnchor target={0}>
+                <ARAnchor 
+                    target={0}
+                    onAnchorFound={() => anchorFound()}
+                    onAnchorLost={() => anchorLost()}
+                >
                 <mesh>
-                    <boxGeometry args={[1, 1, 0.1]} />
+                    <boxGeometry args={[1, 1, 11]} />
                     <meshStandardMaterial color="orange" />
                 </mesh>
                 </ARAnchor>
             </ARView>
+            {showText && (
+                <h1>Test Text</h1>
+            )}
         </div>
     )
 }
